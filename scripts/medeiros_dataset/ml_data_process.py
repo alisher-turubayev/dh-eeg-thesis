bf6864016aa219a3b4576c22d3ad5cc44c4104c9
Alisher Turubayev <alisher.turubayev@gmail.com>
2023-09-25 23:47:53 +0200

    ML data extract script completed

---
+++ scripts/medeiros_dataset/ml_data_process.py
@@ -0,0 +1,207 @@
+# Automated Machine Learning data preparation script for BASE Mental
+#   Effort Monitoring Dataset by Medeiros et al (2021)
+#
+# Full reference:
+#
+# Medeiros, J., Couceiro, R., Duarte, G., Durães, J., Castelhano, J.,
+#   Duarte, C., Castelo-Branco, M., Madeira, H., de Carvalho, P., &
+#   Teixeira, C. (2021). Can EEG Be Adopted as a Neuroscience Reference
+#   for Assessing Software Programmers' Cognitive Load? Sensors, 21(7),
+#   2338. https://doi.org/10.3390/s21072338
+#
+# Work completed as part of the Master's Thesis for M.Sc. Digital Health
+#   @ Hasso-Plattner Institute, University of Potsdam, Germany
+#
+# Authors: Alisher Turubayev, Fabian Stolp (PhD supervisor)
+#
+# Goals:
+#   open .set files in a specified directory, do feature extraction,
+#   normalization, and transformation, and pack the data into parquet
+#   file(s)
+#
+# Usage:
+#   1. Install required packages:
+#       pandas
+#       mne
+#       scikit-learn
+#       antropy
+#       matlab.engine
+#       scipy
+#   2. Adjust DATA_PATH / STORAGE_PATH as needed
+#   3. Ensure your Matlab installation is on path or is in default location. 
+#       See https://www.mathworks.com/help/matlab/matlab-engine-for-python.html
+#   3. Run the script with:
+#       python3 ./medeiros_raw_processing.py
+#
+# Notes:
+#   1. Mean of normalized signal was done as mean of min-max scaled signal
+#   2. The number of power ratio features is increased to 42, 
+#       as there are 7 * 6 pairs of frequency bands. In total, there are 68
+#       uni-channel features instead of declared 48 in the paper.
+#   3. `Average frequency as estimation of mean frequency of the PSD` (p.11)
+#       did not make sense to us, so we opted to calculate mean frequency for each
+#       frequency band, and then take an average of that.
+#   4. The number of differential and absolute asymmetry features is increased to 
+#       168 from 126. We could not understand what 9 pairs were used for the calculation,
+#       so we opted to do these calculations with the 12 pairs described in 
+#       Lin et al. (2010) http://ieeexplore.ieee.org/document/5458075/.
+# ------------------------------------------
+import os
+from datetime import datetime
+import mne
+import numpy as np
+import pandas as pd
+import matlab.engine as mt
+
+import warnings
+warnings.filterwarnings("error")
+
+from ml_utils import extract_features, normalize_val, split_into_segments
+
+DATA_PATH = '~/data/medeiros_raw_extracted'
+STORAGE_PATH = './data/medeiros/medeiros_processed'
+
+# Participant IDs
+PARTICIPANTS = ['S01', 'S03', 'S04', 'S05', 'S07', 'S08', 'S10', 'S11', 'S12',
+    'S13', 'S14', 'S16', 'S17', 'S18', 'S19', 'S20', 'S21', 'S22', 'S23', 'S24',
+    'S25', 'S26', 'S27', 'S28', 'S29', 'S30']
+
+# File associations
+FILE_SUFFIXES = {
+    'task1': 'R01', 
+    'task2': 'R02', 
+    'task3': 'R03'
+    }
+# One-hot encoding for labels
+TASK_CODES = {
+    'control': [1, 0, 0, 0], 
+    'task1': [0, 1, 0, 0], 
+    'task2': [0, 0, 1, 0], 
+    'task3': [0, 0, 0, 1]
+    }
+TASK_COL_LABELS = ['sample_index', 'label0', 'label1', 'label2', 'label3']
+
+start_time = datetime.now()
+
+# Expand the user ~ to absolute path - needed so that the os.path.isfile check works
+if DATA_PATH[0] == '~':
+    DATA_PATH = os.path.expanduser(DATA_PATH)
+
+# Make sure storage path exists
+os.makedirs(STORAGE_PATH, exist_ok = True)
+
+# Remove files where to store if exists (we are overriding it)
+for file in os.listdir(STORAGE_PATH):
+    os.remove(os.path.join(STORAGE_PATH, file))
+
+# Initialize matlab's engine to execute meanfreq function
+eng = mt.start_matlab()
+
+for participant_name in PARTICIPANTS:
+    print(f'Processing participant {participant_name}')
+    participant_start_time = datetime.now()
+    for task_key in FILE_SUFFIXES:
+        file_name = participant_name + FILE_SUFFIXES[task_key] + '_an.set'
+        full_path = os.path.join(DATA_PATH, participant_name, file_name)
+
+        print(f'  Processing file {file_name}...')
+
+        if os.path.isfile(full_path) == False:
+            print(f'  No file {full_path} was found. Continuing...')
+            break
+
+        # Preload file
+        raw = mne.io.read_raw_eeglab(full_path, preload = True, verbose = False)
+        # Convert to dataframe and drop unneeded time column 
+        #   (the converted dataframe is time-synced anyway)
+        df = raw.to_data_frame() 
+        df.drop('time', axis = 1, inplace = True)
+        # Extract events information from EEGLAB (timestamps of events)
+        events, _ = mne.events_from_annotations(raw, verbose = False)
+        # Important to close raw file
+        raw.close()
+
+        # We know that the order of events is the same - see Medeiros et al. (2021) - Figure 1
+        # Thus, we can just select all subsections that are of interest for the scirpting
+        #   from the beginning of dataset to end of comprehension task
+        timeframe_control_baseline = (events[0][0], events[1][0])
+        timeframe_control = (events[1][0], events[2][0])
+        timeframe_task_baseline = (events[2][0], events[3][0])
+        timeframe_task = (events[3][0], events[4][0])
+
+        #### Feature Extraction
+        # Extract subsections and extract features of interest
+        control_baseline_features = extract_features(df.iloc[timeframe_control_baseline[0]:timeframe_control_baseline[1]], eng)
+        control_features = extract_features(df.iloc[timeframe_control[0]:timeframe_control[1]], eng)
+        task_baseline_features = extract_features(df.iloc[timeframe_task_baseline[0]:timeframe_task_baseline[1]], eng)
+        task_features = extract_features(df.iloc[timeframe_task[0]:timeframe_task[1]], eng)
+        del df
+
+        #### Feature Normalization
+        for column in control_baseline_features.columns:
+            column_mean = control_baseline_features[column].mean()
+            control_features[column] = control_features[column].apply(lambda x: normalize_val(x, column_mean))
+
+        for column in task_baseline_features.columns:
+            column_mean = task_baseline_features[column].mean()
+            task_features[column] = task_features[column].apply(lambda x: normalize_val(x, column_mean))
+
+        # Known to produce 0s because power can be close to 0 and therefore eval to 0 in some cases
+        # Floating point shenanigans :3
+        control_features = control_features.fillna(0.0)
+        task_features = task_features.fillna(0.0)
+
+        #### Feature Transformation
+        control = pd.DataFrame()
+        segments = split_into_segments(control_features)
+        for segment in segments:
+            seg_max = segment.max(skipna = True, numeric_only = True).to_frame().T
+            seg_max = seg_max.add_prefix('max_')
+            seg_min = segment.min(skipna = True, numeric_only = True).to_frame().T
+            seg_min = seg_min.add_prefix('min_')
+            seg_mean = segment.mean(skipna = True, numeric_only = True).to_frame().T
+            seg_mean = seg_mean.add_prefix('mean_')
+            seg_std = segment.std(skipna = True, numeric_only = True).to_frame().T
+            seg_std = seg_std.add_prefix('std_')
+            seg_median = segment.median(skipna = True, numeric_only = True).to_frame().T
+            seg_median = seg_median.add_prefix('median_')
+
+            segment_agg = pd.concat([seg_max, seg_min, seg_mean, seg_std, seg_median], axis = 1)
+            control = pd.concat([control, segment_agg], ignore_index = True)
+        # Add labels
+        control['label0'] = 1
+        control['label1'] = 0
+        control['label2'] = 0
+        control['label3'] = 0
+
+        task = pd.DataFrame()
+        segments = split_into_segments(task_features)
+        for segment in segments:
+            seg_max = segment.max(skipna = True, numeric_only = True).to_frame().T
+            seg_max = seg_max.add_prefix('max_')
+            seg_min = segment.min(skipna = True, numeric_only = True).to_frame().T
+            seg_min = seg_min.add_prefix('min_')
+            seg_mean = segment.mean(skipna = True, numeric_only = True).to_frame().T
+            seg_mean = seg_mean.add_prefix('mean_')
+            seg_std = segment.std(skipna = True, numeric_only = True).to_frame().T
+            seg_std = seg_std.add_prefix('std_')
+            seg_median = segment.median(skipna = True, numeric_only = True).to_frame().T
+            seg_median = seg_median.add_prefix('median_')
+            segment_agg = pd.concat([seg_max, seg_min, seg_mean, seg_std, seg_median], axis = 1)
+            task = pd.concat([task, segment_agg], ignore_index = True)
+        # Add labels
+        task['label0'] = 0
+        task['label1'] = TASK_CODES[task_key][1]
+        task['label2'] = TASK_CODES[task_key][2]
+        task['label3'] = TASK_CODES[task_key][3]
+
+        pd.concat([control, task], join = 'inner', ignore_index = True).to_parquet(os.path.join(STORAGE_PATH, participant_name + FILE_SUFFIXES[task_key] + '.parquet'))
+    
+    participant_end_time = datetime.now()
+    print(f'-> Processing for participant {participant_name} finished. Time to execute: {participant_end_time - participant_start_time}')
+
+eng.exit()
+end_time = datetime.now()
+
+print('')
+print(f'Finished data processing. Total time to execute: {end_time - start_time}')
---
+++ scripts/medeiros_dataset/ml_utils.py
@@ -0,0 +1,265 @@
+# Utility functions for automated data processing of the BASE Mental
+#   Effort Monitoring Dataset by Medeiros et al (2021)
+#
+# Full reference:
+#
+# Medeiros, J., Couceiro, R., Duarte, G., Durães, J., Castelhano, J.,
+#   Duarte, C., Castelo-Branco, M., Madeira, H., de Carvalho, P., &
+#   Teixeira, C. (2021). Can EEG Be Adopted as a Neuroscience Reference
+#   for Assessing Software Programmers' Cognitive Load? Sensors, 21(7),
+#   2338. https://doi.org/10.3390/s21072338
+#
+# Work completed as part of the Master's Thesis for M.Sc. Digital Health
+#   @ Hasso-Plattner Institute, University of Potsdam, Germany
+#
+# Authors: Alisher Turubayev, Fabian Stolp (PhD supervisor)
+# ------------------------------------------
+import antropy as ant
+import pandas as pd
+import numpy as np
+import matlab.engine as mt
+import pandas as pd
+import scipy.stats as scs
+import scipy.signal as sig
+from scipy.integrate import simpson
+
+# Electrode pairs as defined in Lin et al. (2010) http://ieeexplore.ieee.org/document/5458075/
+ELECTRODE_PAIRS = [('fp1', 'fp2'),
+    ('f7', 'f8'),
+    ('f3', 'f4'),
+    ('ft7', 'ft8'),
+    ('fc3', 'fc4'),
+    ('t7', 't8'),
+    ('p7', 'p8'),
+    ('c3', 'c4'),
+    ('tp7', 'tp8'),
+    ('cp3', 'cp4'),
+    ('p3', 'p4'),
+    ('o1', 'o2')]
+
+SAMPLE_RATE = 1000 # in Hz
+WINDOW_SIZE = 1 # in seconds
+OVERLAP_RATIO = 0.5 # from 0 to 1
+N_SEGMENTS = 4 # as is in Medeiros et al. (2021)
+
+def normalize_val(x, x_hat):
+    """
+    Normalize value with the formula:
+    (`x` - `x_hat`) / `x_hat`
+
+    See Medeiros et al. (2021) p. 12
+    """
+    return (x - x_hat) / x_hat
+
+def split_into_segments(df):
+    """
+    Split a `pandas.DataFrame` into segments. The number of segments is defined by `N_SEGMENTS`
+    and is set to 4 by default.
+
+    See Medeiros et al. (2021) p. 12
+    """
+    nperseg = int(df.shape[0] / N_SEGMENTS)
+    
+    segment_start = 0
+    segments = []
+    for _ in range(N_SEGMENTS):
+        if segment_start + nperseg > df.shape[0]:
+            segments.append(df.iloc[segment_start:df.shape[0]])
+        else: 
+            segments.append(df.iloc[segment_start:nperseg])
+            segment_start += nperseg
+
+    return segments
+
+def assemble_windows_array(s, w_size, ovlp):
+    """
+    Creates a list of windows with size `w_size` and overlap of `ovlp` elements from splicable `s`
+
+    Parameters:
+    `s`: array-like of elements to slice into windows
+    `w_size`: size of the window in # of elements
+    `ovlp`: size of the overlap in # of elements
+
+    Returns:
+    `windows`: a list of windows of size `w_size`
+    """
+    windows = []
+    
+    lb = 0
+    rb = w_size
+
+    while True:
+        if rb > len(s):
+            windows.append(s[lb:len(s)])
+            break
+        windows.append(s[lb:rb])
+        lb += ovlp
+        rb += ovlp
+
+    return windows
+
+def extract_features(df_unprocessed, eng):
+    """
+    Extract all features as in Medeiros et al. (2021) pp. 10-12
+
+    Notes: 
+    1. Mean of normalized signal was done as mean of min-max scaled signal
+    2. The number of power ratio features is increased to 42, 
+        as there are 7 * 6 pairs of frequency bands. In total, there are 68
+        uni-channel features instead of declared 48 in the paper.
+    3. `Average frequency as estimation of mean frequency of the PSD` (p.11)
+        did not make sense to us, so we opted to calculate mean frequency for each
+        frequency band, and then take an average of that.
+    4. The number of differential and absolute asymmetry features is increased to 
+        168 from 126. We could not understand what 9 pairs were used for the calculation,
+        so we opted to do these calculations with the 12 pairs described in 
+        Lin et al. (2010) http://ieeexplore.ieee.org/document/5458075/.
+    """
+    # All unichannel features of all channels across section (baseline/control/task)
+    df_uf = pd.DataFrame()
+    # All multichannel features of all channels across section (baseline/control_task)
+    df_mf = pd.DataFrame()
+
+    # Calculate n_per_segments & overlap size
+    nperseg = WINDOW_SIZE * SAMPLE_RATE
+    overlap = int(nperseg * OVERLAP_RATIO)
+
+    # Uni-channel features
+    for chan_name in df_unprocessed.columns:
+        chan_windows = assemble_windows_array(df_unprocessed[chan_name], nperseg, overlap)
+
+        df_chan_uf = pd.DataFrame()
+        for window in chan_windows:
+            f_window = {}
+            ## Time-Domain Uni-Channel
+            f_window['f_mean'] = np.mean(window)
+            f_window['f_norm_mean'] = np.mean((window - np.min(window)) / (np.max(window) - np.min(window)))
+            f_window['f_var'] = np.var(window)
+            f_window['f_scew'] = scs.skew(window)
+            f_window['f_kurt'] = scs.kurtosis(window)
+            f_window['f_mob'], f_window['f_comp'] = ant.hjorth_params(window)
+
+            ## Frequency-Domain Uni-Channel
+            # fs - signal frequency, 
+            freqs, psd = sig.welch(window, fs = SAMPLE_RATE, nperseg = min(nperseg, len(window)))
+            # Frequencies of interest from 0 Hz to 90 Hz (p. 11)
+            idx_interest = np.logical_and(freqs >= 0, freqs <= 90)
+            # Frequency resolution
+            freqs_res = freqs[1] - freqs[0]
+            # Total power
+            f_window['f_total_power'] = simpson(psd[idx_interest], dx = freqs_res)
+            # Find freqs of interest
+            idx_delta = np.logical_and(freqs >= 0.0, freqs <= 4.0) 
+            idx_theta = np.logical_and(freqs >= 4.0, freqs <= 8.0)
+            idx_alpha = np.logical_and(freqs >= 8.0, freqs <= 13.0)
+            idx_beta = np.logical_and(freqs >= 13.0, freqs <= 30.0)
+            idx_low_gamma = np.logical_and(freqs >= 30.0, freqs <= 50.0)
+            idx_mid_gamma = np.logical_and(freqs >= 50.0, freqs <= 70.0)
+            idx_hi_gamma = np.logical_and(freqs >= 70.0, freqs <= 90.0)
+            # Absolute/relative power
+            f_window['f_delta_power'] = simpson(psd[idx_delta], dx = freqs_res)
+            f_window['f_delta_rel_power'] = f_window['f_delta_power'] / f_window['f_total_power']
+            f_window['f_theta_power'] = simpson(psd[idx_theta], dx = freqs_res)
+            f_window['f_theta_rel_power'] = f_window['f_theta_power'] / f_window['f_total_power']
+            f_window['f_alpha_power'] = simpson(psd[idx_alpha], dx = freqs_res)
+            f_window['f_alpha_rel_power'] = f_window['f_alpha_power'] / f_window['f_total_power']
+            f_window['f_beta_power'] = simpson(psd[idx_theta], dx = freqs_res)
+            f_window['f_beta_rel_power'] = f_window['f_beta_power'] / f_window['f_total_power']
+            f_window['f_low_gamma_power'] = simpson(psd[idx_low_gamma], dx = freqs_res)
+            f_window['f_low_gamma_rel_power'] = f_window['f_low_gamma_power'] / f_window['f_total_power']
+            f_window['f_mid_gamma_power'] = simpson(psd[idx_mid_gamma], dx = freqs_res)
+            f_window['f_mid_gamma_rel_power'] = f_window['f_mid_gamma_power'] / f_window['f_total_power']
+            f_window['f_hi_gamma_power'] = simpson(psd[idx_hi_gamma], dx = freqs_res)
+            f_window['f_hi_gamma_rel_power'] = f_window['f_hi_gamma_power'] / f_window['f_total_power']
+
+            # pwr -> power ratio
+            f_window['f_pwr_dt'] = f_window['f_delta_power'] / f_window['f_theta_power']
+            f_window['f_pwr_da'] = f_window['f_delta_power'] / f_window['f_alpha_power']
+            f_window['f_pwr_db'] = f_window['f_delta_power'] / f_window['f_beta_power']
+            f_window['f_pwr_dlg'] = f_window['f_delta_power'] / f_window['f_low_gamma_power']
+            f_window['f_pwr_dmg'] = f_window['f_delta_power'] / f_window['f_mid_gamma_power']
+            f_window['f_pwr_dhg'] = f_window['f_delta_power'] / f_window['f_hi_gamma_power']
+            f_window['f_pwr_td'] = f_window['f_theta_power'] / f_window['f_delta_power']
+            f_window['f_pwr_ta'] = f_window['f_theta_power'] / f_window['f_alpha_power']
+            f_window['f_pwr_tb'] = f_window['f_theta_power'] / f_window['f_beta_power']
+            f_window['f_pwr_tlg'] = f_window['f_theta_power'] / f_window['f_low_gamma_power'] 
+            f_window['f_pwr_tmg'] = f_window['f_theta_power'] / f_window['f_mid_gamma_power']
+            f_window['f_pwr_thg'] = f_window['f_theta_power'] / f_window['f_hi_gamma_power']
+            f_window['f_pwr_ad'] = f_window['f_alpha_power'] / f_window['f_delta_power']
+            f_window['f_pwr_at'] = f_window['f_alpha_power'] / f_window['f_theta_power']
+            f_window['f_pwr_ab'] = f_window['f_alpha_power'] / f_window['f_beta_power']
+            f_window['f_pwr_alg'] = f_window['f_alpha_power'] / f_window['f_low_gamma_power']
+            f_window['f_pwr_amg'] = f_window['f_alpha_power'] / f_window['f_mid_gamma_power']
+            f_window['f_pwr_ahg'] = f_window['f_alpha_power'] / f_window['f_hi_gamma_power']
+            f_window['f_pwr_bd'] = f_window['f_beta_power'] / f_window['f_delta_power']
+            f_window['f_pwr_bt'] = f_window['f_beta_power'] / f_window['f_theta_power']
+            f_window['f_pwr_ba'] = f_window['f_beta_power'] / f_window['f_alpha_power']
+            f_window['f_pwr_blg'] = f_window['f_beta_power'] / f_window['f_low_gamma_power']
+            f_window['f_pwr_bmg'] = f_window['f_beta_power'] / f_window['f_mid_gamma_power']
+            f_window['f_pwr_bhg'] = f_window['f_beta_power'] / f_window['f_hi_gamma_power']
+            f_window['f_pwr_lgd'] = f_window['f_low_gamma_power'] / f_window['f_delta_power']
+            f_window['f_pwr_lgt'] = f_window['f_low_gamma_power'] / f_window['f_theta_power']
+            f_window['f_pwr_lga'] = f_window['f_low_gamma_power'] / f_window['f_alpha_power']
+            f_window['f_pwr_lgb'] = f_window['f_low_gamma_power'] / f_window['f_beta_power']
+            f_window['f_pwr_lgmg'] = f_window['f_low_gamma_power'] / f_window['f_mid_gamma_power']
+            f_window['f_pwr_lghg'] = f_window['f_low_gamma_power'] / f_window['f_hi_gamma_power']
+            f_window['f_pwr_mgd'] = f_window['f_mid_gamma_power'] / f_window['f_delta_power']
+            f_window['f_pwr_mgt'] = f_window['f_mid_gamma_power'] / f_window['f_theta_power']
+            f_window['f_pwr_mga'] = f_window['f_mid_gamma_power'] / f_window['f_alpha_power']
+            f_window['f_pwr_mgb'] = f_window['f_mid_gamma_power'] / f_window['f_beta_power']
+            f_window['f_pwr_mglg'] = f_window['f_mid_gamma_power'] / f_window['f_low_gamma_power']
+            f_window['f_pwr_mghg'] = f_window['f_mid_gamma_power'] / f_window['f_hi_gamma_power']
+            f_window['f_pwr_hgd'] = f_window['f_hi_gamma_power'] / f_window['f_delta_power']
+            f_window['f_pwr_hgt'] = f_window['f_hi_gamma_power'] / f_window['f_theta_power']
+            f_window['f_pwr_hga'] = f_window['f_hi_gamma_power'] / f_window['f_alpha_power']
+            f_window['f_pwr_hgb'] = f_window['f_hi_gamma_power'] / f_window['f_beta_power']
+            f_window['f_pwr_hglg'] = f_window['f_hi_gamma_power'] / f_window['f_low_gamma_power']
+            f_window['f_pwr_hgmg'] = f_window['f_hi_gamma_power'] / f_window['f_mid_gamma_power']
+            # Average of mean freq
+            mf_d = eng.meanfreq(psd[idx_delta], SAMPLE_RATE)
+            mf_t = eng.meanfreq(psd[idx_theta], SAMPLE_RATE)
+            mf_a = eng.meanfreq(psd[idx_alpha], SAMPLE_RATE)
+            mf_b = eng.meanfreq(psd[idx_beta], SAMPLE_RATE)
+            mf_lg = eng.meanfreq(psd[idx_low_gamma], SAMPLE_RATE)
+            mf_mg = eng.meanfreq(psd[idx_mid_gamma], SAMPLE_RATE)
+            mf_hg = eng.meanfreq(psd[idx_hi_gamma], SAMPLE_RATE)
+
+            f_window['f_av_meanfreq'] = np.mean([mf_d, mf_t, mf_a, mf_b, mf_lg, mf_mg, mf_hg])
+            # Brain indicies
+            f_window['f_brain_index1'] = f_window['f_beta_power'] / (f_window['f_theta_power'] + f_window['f_alpha_power'])
+            f_window['f_brain_index2'] = f_window['f_theta_power'] / (f_window['f_beta_power'] + f_window['f_alpha_power'])
+            # Finding what frequency corresponds to maximum alpha frequency
+            f_window['f_peak_alpha'] = np.argmax(psd[idx_alpha])
+
+            df_chan_uf = pd.concat([df_chan_uf, pd.DataFrame(f_window, index = [0])], ignore_index = True)
+
+        df_chan_uf = df_chan_uf.add_prefix(chan_name.lower() + '_', axis = 1)
+        df_uf = pd.concat([df_uf, df_chan_uf], axis = 1)
+
+    ## Multi-channel features
+    for _, row in df_uf.iterrows():
+        f_pair = {}
+        for pair in ELECTRODE_PAIRS:
+            x, y = pair
+            # Differential asymmetry calculations
+            f_pair[x + '_' +  y + '_f_da_d'] = row[x + '_f_delta_power'] - row[y + '_f_delta_power']
+            f_pair[x + '_' +  y + '_f_da_t'] = row[x + '_f_theta_power'] - row[y + '_f_theta_power']
+            f_pair[x + '_' +  y + '_f_da_a'] = row[x + '_f_alpha_power'] - row[y + '_f_alpha_power']
+            f_pair[x + '_' +  y + '_f_da_b'] = row[x + '_f_beta_power'] - row[y + '_f_beta_power']
+            f_pair[x + '_' +  y + '_f_da_lg'] = row[x + '_f_low_gamma_power'] - row[y + '_f_low_gamma_power']
+            f_pair[x + '_' +  y + '_f_da_mg'] = row[x + '_f_mid_gamma_power'] - row[y + '_f_mid_gamma_power']
+            f_pair[x + '_' +  y + '_f_da_hg'] = row[x + '_f_hi_gamma_power'] - row[y + '_f_hi_gamma_power']
+            # Absolute asymmetry calculations
+            f_pair[x + '_' +  y + '_f_aa_d'] = row[x + '_f_delta_power'] / row[y + '_f_delta_power']
+            f_pair[x + '_' +  y + '_f_aa_t'] = row[x + '_f_theta_power'] / row[y + '_f_theta_power']
+            f_pair[x + '_' +  y + '_f_aa_a'] = row[x + '_f_alpha_power'] / row[y + '_f_alpha_power']
+            f_pair[x + '_' +  y + '_f_aa_b'] = row[x + '_f_beta_power'] / row[y + '_f_beta_power']
+            f_pair[x + '_' +  y + '_f_aa_lg'] = row[x + '_f_low_gamma_power'] / row[y + '_f_low_gamma_power']
+            f_pair[x + '_' +  y + '_f_aa_mg'] = row[x + '_f_mid_gamma_power'] / row[y + '_f_mid_gamma_power']
+            f_pair[x + '_' +  y + '_f_aa_hg'] = row[x + '_f_hi_gamma_power'] / row[y + '_f_hi_gamma_power']
+
+        f_pair['f_brainbeat'] = row['fz_f_theta_power'] / row['pz_f_alpha_power']
+        df_mf = pd.concat([df_mf, pd.DataFrame(f_pair, index = [0])], ignore_index = True)
+
+    df = pd.concat([df_uf, df_mf], axis = 1)
+    return df
